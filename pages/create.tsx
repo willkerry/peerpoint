d0373ff7c4a4d7f338d92d89b0bb1944dc85699e
Will Kerry <mail@example.com>
2022-07-21 15:58:25 +0100

    Language features in create interface

--- pages/create.tsx
+++ pages/create.tsx
@@ -5,11 +5,13 @@ import Layout from "../components/layout";
 import Meta from "../components/meta";
 import CodeEditor from "../components/code-editor";
 import LanguageSelect from "../components/language-select";
+import { type Language, usefulLanguages } from "../@types/Language";
 
-const Draft: React.FC = () => {
-  const [title, setTitle] = useState("");
-  const [skeleton, setSkeleton] = useState("");
-  const [expectedOutput, setExpectedOutput] = useState("");
+const Create: React.FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const [language, setLanguage] = useState<Language["id"] | null>(null);
+  const [skeleton, setSkeleton] = useState<string>("");
+  const [expectedOutput, setExpectedOutput] = useState<string>("");
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -41,11 +43,15 @@ const Draft: React.FC = () => {
               type="text"
               value={title}
             />
-            {/* Not controlled! Just for show. */}
-            <LanguageSelect required label="Language" />
-
+            <LanguageSelect
+              required
+              label="Language"
+              value={language}
+              onChange={(e) => setLanguage(Number(e.target.value))}
+            />
             <CodeEditor
               label="Skeleton"
+              language={usefulLanguages.find((l) => l.id === language)?.cm}
               description="
                 This is what appears in students’ code editors. Include 
                 instructions (commented out), and any necessary boilerplate or 
@@ -78,4 +84,4 @@ const Draft: React.FC = () => {
   );
 };
 
-export default Draft;
+export default Create;
