f234f49899f3e63c4e488faccba75da148dc7385
Will Kerry <mail@example.com>
2022-07-23 19:24:12 +0100

    Implement more rate limiting

--- pages/api/c/index.ts
+++ pages/api/c/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import { getSession } from "next-auth/react";
+import { standardLimiter } from "../../../utils/rate-limit";
 
 // POST /api/c
 export default async function handle(
@@ -8,8 +9,16 @@ export default async function handle(
   res: NextApiResponse
 ) {
   const { title, expectedOutput, skeleton, language = 1 } = req.body;
-
   const session = await getSession({ req });
+
+  try {
+    await standardLimiter.check(res, 10, "CACHE_TOKEN");
+  } catch (e) {
+    console.warn(e);
+    res.status(429).send("Rate limited");
+    return;
+  }
+
   if (session) {
     const result = await prisma.challenge.create({
       data: {
