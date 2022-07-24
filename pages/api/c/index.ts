f40b28f0af6d76d887f481e920e2b1c476376dc2
Will Kerry <mail@example.com>
2022-07-24 14:46:16 +0100

    Static front page with dynamic fetch

--- pages/api/c/index.ts
+++ pages/api/c/index.ts
@@ -8,9 +8,6 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { title, expectedOutput, skeleton, language = 1 } = req.body;
-  const session = await getSession({ req });
-
   try {
     await standardLimiter.check(res, 10, "CACHE_TOKEN");
   } catch (e) {
@@ -19,19 +16,42 @@ export default async function handle(
     return;
   }
 
-  if (session) {
-    const result = await prisma.challenge.create({
-      data: {
-        title: title,
-        expectedOutput: expectedOutput,
-        skeleton: skeleton,
-        language: language,
+  if (req.method === "POST") {
+    const { title, expectedOutput, skeleton, language = 1 } = req.body;
+    const session = await getSession({ req });
+
+    if (session) {
+      const result = await prisma.challenge.create({
+        data: {
+          title: title,
+          expectedOutput: expectedOutput,
+          skeleton: skeleton,
+          language: language,
+          published: true,
+          author: { connect: { email: session?.user?.email } },
+        },
+      });
+      res.json(result);
+    } else {
+      res.status(401).send({ message: "Unauthorized" });
+    }
+  } else if (req.method === "GET") {
+    const feed = await prisma.challenge.findMany({
+      where: {
         published: true,
-        author: { connect: { email: session?.user?.email } },
       },
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 10,
     });
-    res.json(result);
-  } else {
-    res.status(401).send({ message: "Unauthorized" });
+    res.json(feed);
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,23 @@
-import { Alert, Grid, Stack } from "@mantine/core";
+import { Alert, Grid, Skeleton, Stack } from "@mantine/core";
 import type { Challenge } from "@prisma/client";
-import type { GetServerSideProps } from "next";
 import React from "react";
+import useSWR, { SWRResponse } from "swr";
 import ChallengePreview from "../components/challenge-preview";
 import FindChallenge from "../components/inputs/find-challenge";
 import Layout from "../components/layout/layout";
 import Meta from "../components/meta";
-import prisma from "../lib/prisma";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.challenge.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      author: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
-  return {
-    props: { feed: JSON.parse(JSON.stringify(feed)) },
-  };
-};
+import getter from "../lib/getter";
 
 type Props = {
   feed: Challenge[];
 };
 
-const Blog: React.FC<Props> = (props) => {
+const Blog: React.FC<Props> = () => {
+  const { data, error }: SWRResponse<Challenge[] | any> = useSWR(
+    "/api/c",
+    getter
+  );
+
   return (
     <Layout>
       <Meta title="Peerpoint" />
@@ -44,11 +31,28 @@ const Blog: React.FC<Props> = (props) => {
         </Alert>
 
         <Grid>
-          {props.feed.map((post) => (
-            <Grid.Col md={3} xs={6} key={post.id}>
-              <ChallengePreview post={post} />
-            </Grid.Col>
-          ))}
+          {data && !error ? (
+            data.map((post) => (
+              <Grid.Col md={3} xs={6} key={post.id}>
+                <ChallengePreview post={post} />
+              </Grid.Col>
+            ))
+          ) : (
+            <>
+              <Grid.Col md={3} xs={6}>
+                <Skeleton height={200} />
+              </Grid.Col>
+              <Grid.Col md={3} xs={6}>
+                <Skeleton height={180} />
+              </Grid.Col>
+              <Grid.Col md={3} xs={6}>
+                <Skeleton height={190} />
+              </Grid.Col>
+              <Grid.Col md={3} xs={6}>
+                <Skeleton height={210} />
+              </Grid.Col>
+            </>
+          )}
         </Grid>
       </Stack>
     </Layout>
