44ea067c03426ec154a13dd0bbb741c69bdd3f64
Will Kerry <mail@example.com>
2022-07-24 19:31:17 +0100

    Favicon

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -5,12 +5,20 @@ interface Props {
   description?: string;
 }
 
-const Meta = ({ title = "Peerpoint" }: Props) => {
-  return (
-    <Head>
-      <title>{title}</title>
-    </Head>
-  );
-};
+const Meta = ({ title = "Peerpoint" }: Props) => (
+  <Head>
+    <title>{title}</title>
+
+    <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+    <link rel="icon" href="/favicon/icon.svg" type="image/svg+xml" />
+    <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/favicon/manifest.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/favicon/safari-pinned-tab.svg"
+      color="#000000"
+    />
+  </Head>
+);
 
 export default Meta;
