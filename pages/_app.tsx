b5172a605c56e22f537bcb83f106fff132bd748f
Will Kerry <mail@example.com>
2022-07-25 09:37:06 +0100

    Inter, instead of Segoe weirdness

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, MantineThemeOverride } from "@mantine/core";
+import "@fontsource/inter/variable-full.css";
+
+const peerpointTheme: MantineThemeOverride = {
+  primaryColor: "orange",
+  fontFamily:
+    "Inter, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji",
+};
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={peerpointTheme} withGlobalStyles withNormalizeCSS>
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
