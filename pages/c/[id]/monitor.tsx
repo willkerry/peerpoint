c4c51449ebd87b2441511fece7e7a0642864104b
Will Kerry <mail@example.com>
2022-07-26 18:58:40 +0100

    Monitoring animation

--- pages/c/[id]/monitor.tsx
+++ pages/c/[id]/monitor.tsx
@@ -1,10 +1,12 @@
 import {
-  RingProgress,
   Title,
   Text,
   Group,
+  Center,
   Stack,
   NativeSelect,
+  useMantineTheme,
+  TextInput,
 } from "@mantine/core";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -12,16 +14,17 @@ import useSWR from "swr";
 import IdButton from "../../../components/display/id-button";
 import { Layout, Meta } from "../../../components/layout";
 import fetchMonitoring from "../../../lib/fetchers/fetch-monitoring";
-import { TextInput } from "@mantine/core";
 import DisplayId from "../../../components/display/display-id";
 import { Var } from "../../../components/display/variable";
 import { BugIcon } from "@primer/octicons-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Monitor = () => {
   const router = useRouter();
   const { id } = router.query;
   const [period, setPeriod] = useState({ value: "15", label: "15 minutes" });
   const { data } = useSWR({ id, period: period.value }, fetchMonitoring);
+  const theme = useMantineTheme();
 
   const periods = [
     { label: "1 minute", value: "1" },
@@ -29,6 +32,9 @@ const Monitor = () => {
     { label: "1 hour", value: "60" },
     { label: "7 days", value: "10080" },
   ];
+
+  const r = 140;
+  const circumference = 2 * Math.PI * r;
 
   const label = (
     <Text
@@ -73,26 +79,85 @@ const Monitor = () => {
           }
         />
         <Stack align="center">
-          <RingProgress
-            size={320}
-            sections={[
-              {
-                value: data?.successRate ? data.successRate : 0,
-                color: "green",
-              },
-            ]}
-            roundCaps
-            thickness={20}
-            label={data?.successRate ? label : empty}
-          />
+          <Center
+            sx={{
+              position: "relative",
+              borderRadius: 9999,
+              overflow: "hidden",
+            }}
+          >
+            <AnimatePresence>
+              {data?.successRate ? (
+                <motion.div
+                  key="fraction"
+                  initial={{ opacity: 0, y: -r }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: r }}
+                  style={{ position: "absolute" }}
+                >
+                  {label}
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="empty"
+                  initial={{ opacity: 0, y: -r }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: r }}
+                  style={{ position: "absolute" }}
+                >
+                  {empty}
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <motion.svg
+              width={320}
+              height={320}
+              style={{ transform: "rotate(-90deg)" }}
+            >
+              <circle
+                cx={160}
+                cy={160}
+                r={r}
+                stroke={theme.colors.gray[3]}
+                strokeWidth={20}
+                fill="transparent"
+              />
+
+              <motion.circle
+                cx={160}
+                cy={160}
+                r={r}
+                stroke={theme.colors.orange[5]}
+                fill="transparent"
+                strokeWidth={20}
+                strokeLinecap="round"
+                strokeDashoffset={circumference}
+                initial={{
+                  strokeDasharray: `${0}, ${circumference}`,
+                }}
+                animate={{
+                  strokeDasharray: `${
+                    (Math.max(data?.successRate, 7) * circumference) / 100
+                  }, ${
+                    circumference -
+                    (Math.max(data?.successRate, 7) * circumference) / 100
+                  }`,
+                }}
+                transition={{ type: "spring", stiffness: 40, delay: 0.5 }}
+              />
+            </motion.svg>
+          </Center>
           <Text>
             <Var>{data?.activeStudents?.toLocaleString()}</Var>{" "}
-            {data?.activeStudents > 1 ? "students have" : "student has"}{" "}
-            attemped challenge <DisplayId id={id} /> in the last{" "}
-            <Var>{period.label}</Var>, of whom{" "}
+            {data?.activeStudents !== 1 ? "students have" : "student has"}{" "}
+            attemped challenge{" "}
+            <Var>
+              <DisplayId id={id} />
+            </Var>{" "}
+            in the last <Var>{period.label}</Var>, of whom{" "}
             <Var>{data?.successfulStudents?.toLocaleString()}</Var>{" "}
-            {data?.successfulStudents > 1 ? "have" : "has"} submitted a correct
-            solution.
+            {data?.successfulStudents !== 1 ? "have" : "has"} submitted a
+            correct solution.
           </Text>
         </Stack>
       </Stack>
