559497bcd37416fc3e0963f4955e4ddbd4434afe
Will Kerry <mail@example.com>
2022-07-27 09:14:40 +0100

    Guard clause to prevent bad requests

--- lib/fetchers/fetch-monitoring.ts
+++ lib/fetchers/fetch-monitoring.ts
@@ -8,6 +8,8 @@ const fetchMonitoring: Fetcher<MonitorResponse> = ({
   id: number;
   period: number;
 }) => {
+  if (!Number(id) || !Number(period)) return;
+
   return fetch(
     `/api/c/monitor/${Number(id)}?${new URLSearchParams({
       period: String(period),
