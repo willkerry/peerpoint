3bb8ff229100cd9af9634ac481bf72583e2494d8
Will Kerry <mail@example.com>
2022-07-31 11:28:24 +0100

    Unified exports for display components

---
+++ components/display/index.ts
@@ -0,0 +1,15 @@
+import ChallengePreview from "./challenge-preview";
+import DisplayId from "./display-id";
+import IdButton from "./id-button";
+import InfoModal from "./info-modal";
+import LanguageIndicator from "./language-indicator";
+import { Var } from "./variable";
+
+export {
+  ChallengePreview,
+  DisplayId,
+  IdButton,
+  InfoModal,
+  LanguageIndicator,
+  Var,
+};
