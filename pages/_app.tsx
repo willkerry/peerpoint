3db438293b5eb36ba2b4c8e83d556062546f20c0
Will Kerry <mail@example.com>
2022-07-31 21:36:04 +0100

    Missed `user-scalable=no`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <title>Peerpoint</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </Head>
       <MantineProvider
