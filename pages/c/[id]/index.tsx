8f0956538c4a41b7a5eca5e04b8c75bf9c42e9be
Will Kerry <mail@example.com>
2022-07-31 17:33:51 +0100

    Show execution results in a modal

--- pages/c/[id]/index.tsx
+++ pages/c/[id]/index.tsx
@@ -3,18 +3,18 @@ import {
   Alert,
   Button,
   Code,
-  Drawer,
   Group,
   LoadingOverlay,
   Paper,
   ScrollArea,
   Stack,
   Title,
+  Text,
 } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Router, { useRouter } from "next/router";
-import { AnchorHTMLAttributes, useState } from "react";
+import { AnchorHTMLAttributes, useState, useEffect } from "react";
 import useSWR from "swr";
 import { SubmissionResponse } from "../../../@types/Submission";
 import { CodeEditor } from "../../../components/inputs";
@@ -22,6 +22,8 @@ import { Layout, Meta } from "../../../components/layout/";
 import { deletePost, publishPost, sendExecuteRequest } from "../../../utils";
 import fetchChallenge from "../../../lib/fetchers/fetch-challenge";
 import IdButton from "../../../components/display/id-button";
+import { openModal, openConfirmModal } from "@mantine/modals";
+import { Var } from "../../../components/display";
 
 const Post: React.FC = () => {
   const router = useRouter();
@@ -31,9 +33,37 @@ const Post: React.FC = () => {
   const [userCode, setUserCode] = useState(data?.skeleton);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [output, setOutput] = useState<SubmissionResponse>();
-  const [showOutput, setShowOutput] = useState(false);
+  const [showResult, setShowResult] = useState(false);
 
   const { data: session, status } = useSession();
+
+  useEffect(() => {
+    setShowResult(false);
+    openModal({
+      title: output?.status?.description,
+      children: (
+        <Stack>
+          {output?.status?.id > 3 && (
+            <Alert title={`Error status ${output?.status?.id}`} color="red">
+              {output?.status?.description ?? output?.message}
+            </Alert>
+          )}
+          {output?.status?.id <= 3 && (
+            <Alert color="green">
+              <ScrollArea>
+                {output?.message || "That’s the correct output."}
+              </ScrollArea>
+            </Alert>
+          )}
+          <Code block>
+            <ScrollArea>
+              {output?.stdout ?? output?.stderr ?? output?.compile_output}
+            </ScrollArea>
+          </Code>
+        </Stack>
+      ),
+    });
+  }, [output, showResult]);
 
   if (status === "loading") return <LoadingOverlay visible />;
 
@@ -47,15 +77,39 @@ const Post: React.FC = () => {
       await sendExecuteRequest(data?.id, data?.language, userCode)
     );
     setIsSubmitting(false);
-    setShowOutput(true);
   };
 
   const handleDelete = async (): Promise<void> => {
-    if (confirm("Are you sure you want to delete this item?")) {
-      await deletePost(data?.id);
-      await Router.push("/");
-    }
-    return;
+    await openConfirmModal({
+      title: "Delete challenge",
+      centered: true,
+      children: (
+        <Text size="sm">
+          Are you sure you want to delete <Var>{data?.title}</Var>?
+        </Text>
+      ),
+      labels: {
+        confirm: "Delete challenge",
+        cancel: "No, don’t delete",
+      },
+      confirmProps: { color: "red" },
+      onConfirm: () => {
+        deletePost(data?.id);
+        Router.push("/");
+      },
+    });
+  };
+
+  const handleEdit = (): void => {
+    openModal({
+      title: (
+        <>
+          Edit <Var>{data?.title}</Var>
+        </>
+      ),
+      children: <Alert>Editing is not implemented yet.</Alert>,
+      centered: true,
+    });
   };
 
   return (
@@ -64,7 +118,8 @@ const Post: React.FC = () => {
       <Meta title={data?.title} />
       <Title order={4} mb={12}>
         <Group position="apart" align="center">
-          {data?.title} <IdButton id={data?.id} />
+          {data?.title}
+          <IdButton id={data?.id} />
         </Group>
       </Title>
 
@@ -76,10 +131,9 @@ const Post: React.FC = () => {
           value={data?.skeleton}
           onChange={(value) => setUserCode(value)}
         />
-
-        <Affix>
-          <Paper>
-            <Group p="md">
+        <Affix zIndex={1} position={{ bottom: 15, right: 15 }}>
+          <Paper withBorder p="md">
+            <Group>
               {!data?.published && userHasValidSession && postBelongsToUser && (
                 <Button
                   onClick={() => publishPost(data?.id)}
@@ -89,31 +143,42 @@ const Post: React.FC = () => {
                   Publish
                 </Button>
               )}
-              {userHasValidSession && postBelongsToUser && (
-                <>
-                  <Button
-                    color="red"
-                    variant="outline"
-                    onClick={() => handleDelete()}
-                    disabled={isSubmitting}
-                    compact
-                  >
-                    Delete
-                  </Button>
-                  <Button variant="outline" disabled={isSubmitting} compact>
-                    Edit
-                  </Button>
-                </>
-              )}
-              <Link href={`${data?.id}/monitor`}>
-                <Button<AnchorHTMLAttributes<HTMLAnchorElement>>
-                  variant="default"
-                  component="a"
-                  compact
-                >
-                  Monitor
+              <Button.Group>
+                {userHasValidSession && postBelongsToUser && (
+                  <>
+                    <Button
+                      color="red"
+                      onClick={() => handleDelete()}
+                      disabled={isSubmitting}
+                      compact
+                    >
+                      Delete
+                    </Button>
+                    <Link href={`${data?.id}/monitor`}>
+                      <Button<AnchorHTMLAttributes<HTMLAnchorElement>>
+                        variant="default"
+                        component="a"
+                        compact
+                      >
+                        Monitor
+                      </Button>
+                    </Link>
+                    <Button
+                      variant="default"
+                      disabled={isSubmitting}
+                      onClick={handleEdit}
+                      compact
+                    >
+                      Edit
+                    </Button>
+                  </>
+                )}
+              </Button.Group>
+              {output && (
+                <Button variant="default" onClick={() => setShowResult(true)}>
+                  Result
                 </Button>
-              </Link>
+              )}
               <Button type="submit" form="codexec" loading={isSubmitting}>
                 Run
               </Button>
@@ -121,35 +186,6 @@ const Post: React.FC = () => {
           </Paper>
         </Affix>
       </form>
-      <Drawer
-        position="bottom"
-        opened={showOutput}
-        onClose={() => setShowOutput(false)}
-        title="Output"
-        padding="md"
-        size="xl"
-      >
-        <Stack>
-          {output?.status?.id > 3 && (
-            <Alert title={`Error ${output?.status?.id}`} color="red">
-              {output?.status?.description ?? output?.message}
-              <Code>{output?.compile_output}</Code>
-            </Alert>
-          )}
-          {output?.status?.id <= 3 && (
-            <Alert title={output?.status?.description} color="green">
-              <ScrollArea>
-                {output?.message || "That’s the correct output."}
-              </ScrollArea>
-            </Alert>
-          )}
-          <Code block>
-            <ScrollArea>
-              {output?.stdout ?? output?.stderr ?? output?.compile_output}
-            </ScrollArea>
-          </Code>
-        </Stack>
-      </Drawer>
     </Layout>
   );
 };
