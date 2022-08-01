a095384fcba36ffb5b90bdfbbb445442225a46a2
Will Kerry <mail@example.com>
2022-08-01 16:57:55 +0100

    Better execution output

--- utils/run/handlers.tsx
+++ utils/run/handlers.tsx
@@ -3,7 +3,7 @@ import Router from "next/router";
 import { deletePost } from "../../utils";
 import { Challenge } from "@prisma/client";
 import { Var } from "../../components/display";
-import { Alert, Code, ScrollArea, Stack, Text } from "@mantine/core";
+import { Alert, Code, ScrollArea, Stack, Text, Title } from "@mantine/core";
 import { SubmissionResponse } from "../../@types/Submission";
 
 export function deleteHandler(data: Challenge) {
@@ -45,27 +45,61 @@ export function editHandler(data: Challenge) {
 
 export function resultModal(setShowResult, output: SubmissionResponse) {
   setShowResult(false);
+  const wasError = output?.status?.id > 3;
   openModal({
-    title: output?.status?.description,
+    title: <Title order={4}>Output</Title>,
     children: (
       <Stack>
-        {output?.status?.id > 3 && (
+        {wasError ? (
           <Alert title={`Error status ${output?.status?.id}`} color="red">
             {output?.status?.description ?? output?.message}
           </Alert>
-        )}
-        {output?.status?.id <= 3 && (
-          <Alert color="green">
+        ) : (
+          <Alert title="Success" variant="filled" color="green">
             <ScrollArea>
               {output?.message || "That’s the correct output."}
             </ScrollArea>
           </Alert>
         )}
-        <Code block>
-          <ScrollArea>
-            {output?.stdout ?? output?.stderr ?? output?.compile_output}
-          </ScrollArea>
-        </Code>
+        <Stack spacing={0}>
+          <Title order={6}>
+            {output?.stdout
+              ? "Standard output"
+              : output?.stderr
+              ? "Standard error"
+              : output?.compile_output
+              ? "Compiler output"
+              : ""}
+          </Title>
+          <Text size="sm" color="dimmed">
+            {output?.stdout ? (
+              <>
+                Your program’s <Code>stdout</Code>.
+              </>
+            ) : output?.stderr ? (
+              <>
+                Your program’s <Code>stderr</Code>.
+              </>
+            ) : output?.compile_output ? (
+              "The compiler’s output."
+            ) : (
+              ""
+            )}
+          </Text>
+          <Code
+            mt={12}
+            block
+            sx={(theme) => ({
+              background: theme.colors.dark[8],
+              color: !wasError ? theme.colors.gray[4] : theme.colors.red[3],
+              fontSize: theme.fontSizes.sm,
+            })}
+          >
+            <ScrollArea sx={{ maxHeight: 150 }}>
+              {output?.stdout ?? output?.stderr ?? output?.compile_output}
+            </ScrollArea>
+          </Code>
+        </Stack>
       </Stack>
     ),
   });
