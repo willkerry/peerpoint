f2eec237845472ce48de30f5fdf3cfccf2594d80
Will Kerry <mail@example.com>
2022-08-01 15:32:07 +0100

    Handle errors on challenge routes

--- pages/c/[id]/index.tsx
+++ pages/c/[id]/index.tsx
@@ -1,17 +1,28 @@
-import { Group, LoadingOverlay, Title } from "@mantine/core";
+import {
+  Center,
+  Group,
+  LoadingOverlay,
+  Skeleton,
+  Space,
+  Stack,
+  Title,
+  Text,
+} from "@mantine/core";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { SubmissionResponse } from "../../../@types/Submission";
 import { IdButton } from "../../../components/display";
-import { CodeEditor } from "../../../components/inputs";
+import { CodeEditor, FindChallenge } from "../../../components/inputs";
 import { Layout, Meta } from "../../../components/layout/";
 import fetchChallenge from "../../../lib/fetchers/fetch-challenge";
 import { resultModal } from "../../../utils/run/handlers";
 import RunControls from "../../../components/inputs/run-controls";
 import { useForm } from "@mantine/form";
 import { sendExecuteRequest } from "../../../utils";
+import { IconTrafficCone } from "@tabler/icons";
+import { useMantineTheme } from "@mantine/core";
 
 const Post: React.FC = () => {
   // Get challenge ID from URL
@@ -19,14 +30,14 @@ const Post: React.FC = () => {
   const { id } = router.query;
 
   // Fetch challenge data
-  const { data } = useSWR(id, fetchChallenge);
+  const { data, error } = useSWR(id, fetchChallenge);
 
   // Get user session
   const { data: session } = useSession();
   const userHasSession = Boolean(session);
   const challengeBelongsToUser = session?.user?.email === data?.author?.email;
 
-  // Establish useStates
+  // Initialise useStates
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [output, setOutput] = useState<SubmissionResponse>();
   const [showResult, setShowResult] = useState(false);
@@ -41,7 +52,6 @@ const Post: React.FC = () => {
   // Populate form with challenge skeleton when it loads
   useEffect(() => {
     if (data?.skeleton) form.setFieldValue("userCode", data.skeleton);
-    console.log("useEffect called");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.skeleton]);
 
@@ -67,18 +77,56 @@ const Post: React.FC = () => {
     setShowResult(true);
   };
 
+  const theme = useMantineTheme();
+
+  if (error)
+    return (
+      <Layout>
+        <Stack my={64} spacing="sm">
+          <Center>
+            <IconTrafficCone
+              size={56}
+              stroke={1.5}
+              color={theme.colors.orange[5]}
+            />
+          </Center>
+          <Center>
+            <Title order={1} color="">
+              Challenge not found.
+            </Title>
+          </Center>
+          <Center>
+            <Text color="dimmed">Try another one.</Text>
+          </Center>
+          <Space h="xl" />
+          <Center>
+            <FindChallenge />
+          </Center>
+        </Stack>
+      </Layout>
+    );
   return (
     <Layout>
-      <LoadingOverlay visible={isSubmitting} />
-      <Meta title={data?.title} />
+      <LoadingOverlay visible={isSubmitting || !data} />
+      <Meta title={data?.title ?? "Peerpoint Challenge"} />
 
-      <Title order={4} mb={12}>
-        <Group position="apart" align="center">
-          {data?.title}
-          <IdButton id={data?.id} />
-        </Group>
-      </Title>
-
+      <Group position="apart" noWrap mb={16}>
+        {!data ? (
+          <Skeleton />
+        ) : (
+          <Title
+            order={4}
+            sx={{
+              textOverflow: "ellipsis",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {data?.title}
+          </Title>
+        )}
+        <IdButton id={data?.id} />
+      </Group>
       <form id="exec" onSubmit={form.onSubmit(handleSubmit)}>
         <CodeEditor
           label="Code editor"
