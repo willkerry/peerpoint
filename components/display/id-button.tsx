d4de8622840930bccdeb028da2dbf10ab317686e
Will Kerry <mail@example.com>
2022-08-01 15:32:42 +0100

    Use modal manager for QR modal

--- components/display/id-button.tsx
+++ components/display/id-button.tsx
@@ -1,37 +1,60 @@
-import { Image, Button } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Image, Button, Box, Loader } from "@mantine/core";
 import { getQrCode } from "../../utils";
-import InfoModal from "./info-modal";
 import { URL } from "../../lib/constants";
 import { DownloadIcon } from "@primer/octicons-react";
 import DisplayId from "./display-id";
+import { IconQrcode } from "@tabler/icons";
+import { openModal } from "@mantine/modals";
+import { DisplayIDProps } from "./display-id";
 
-const IdButton = ({ id }) => {
-  const [opened, handlers] = useDisclosure(false);
+const IdButton: React.FC<DisplayIDProps> = ({ id }: DisplayIDProps) => {
   const qrCode = getQrCode(`${URL}/c/${id}`);
-  return (
-    <InfoModal
-      isOpen={opened}
-      setClosed={() => handlers.close()}
-      setOpen={() => handlers.open()}
-      title="Share"
-      variant="default"
-      button={<DisplayId id={id} />}
-    >
-      <Image
-        src={qrCode}
-        alt="QR code for this ID"
-        width={300}
-        height={300}
-        mb={12}
-        withPlaceholder
-      />
+  const title = "QR Code";
+
+  const children = (
+    <>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+        }}
+      >
+        <Image
+          src={qrCode}
+          height={300}
+          width={300}
+          alt="QR code for this ID"
+          mb={12}
+          withPlaceholder
+          placeholder={
+            <Box sx={{ position: "relative" }}>
+              <IconQrcode size={32} style={{ position: "absolute" }} />
+              <Loader size="lg" sx={{ position: "absolute" }} />
+            </Box>
+          }
+        />
+      </Box>
       <a href={qrCode} download="qr-code.png">
         <Button rightIcon={<DownloadIcon />} variant="default">
           Download QR code
         </Button>
       </a>
-    </InfoModal>
+    </>
+  );
+
+  return (
+    <Button
+      compact
+      variant="default"
+      onClick={() =>
+        openModal({
+          title,
+          children,
+        })
+      }
+    >
+      <DisplayId id={id} />
+    </Button>
   );
 };
 
