81edd769edcb20fb7aabb32bb8c81eda9842273e
Will Kerry <mail@example.com>
2022-08-03 10:55:23 +0100

    Better header links

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import { NextLink } from "@mantine/next";
 import { useRouter } from "next/router";
 import { signOut, useSession } from "next-auth/react";
-import { Button, Group, Skeleton } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  Menu,
+  Skeleton,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import {
+  IconInfoCircle,
+  IconLogout,
+  IconPlus,
+  IconSearch,
+  IconTable,
+  IconUser,
+} from "@tabler/icons";
 
 const Header: React.FC = () => {
   const router = useRouter();
@@ -30,12 +47,62 @@ const Header: React.FC = () => {
   if (session) {
     headerLinks = (
       <Group>
-        <Link href="/create" passHref>
-          <Button component="a">Create</Button>
-        </Link>
-        <Button variant="outline" onClick={() => signOut()}>
-          Log out
-        </Button>
+        <Tooltip label="Create a challenge">
+          <ActionIcon
+            component={NextLink}
+            href="/create"
+            variant="filled"
+            color="orange"
+            size="lg"
+          >
+            <IconPlus size={16} stroke={3} />
+          </ActionIcon>
+        </Tooltip>
+
+        <Menu shadow="md" width={200}>
+          <Menu.Target>
+            <ActionIcon component="a" variant="outline" size="lg">
+              <IconUser size={16} stroke={2} />
+            </ActionIcon>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Label>Application</Menu.Label>
+            <Menu.Item
+              href="/admin"
+              component={NextLink}
+              icon={<IconTable size={14} />}
+            >
+              Your challenges
+            </Menu.Item>
+            <Menu.Item
+              href="/about"
+              component={NextLink}
+              icon={<IconInfoCircle size={14} />}
+            >
+              About
+            </Menu.Item>
+            <Menu.Item
+              icon={<IconSearch size={14} />}
+              rightSection={
+                <Text size="xs" color="dimmed">
+                  ⌘K
+                </Text>
+              }
+            >
+              Find a challenge
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Label>Account</Menu.Label>
+            <Menu.Item
+              onClick={() => signOut()}
+              color="red"
+              icon={<IconLogout size={14} />}
+            >
+              Log out
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </Group>
     );
   }
