157e0a3bb0d0a70a500f8970d9c0f890d223d9f4
Will Kerry <mail@example.com>
2022-08-05 09:33:05 +0100

    Allow custom fetcher to append query string

--- lib/fetchers/fetch-challenges.ts
+++ lib/fetchers/fetch-challenges.ts
@@ -1,13 +1,21 @@
 import { Fetcher } from "swr";
 import { Challenge } from "@prisma/client";
 
-const fetchChallenges: Fetcher<Challenge[]> = () => {
-  return fetch("/api/c", {
+/*
+ * Pass `own` argument to fetch only challenges created by the user.
+ * Otherwise, fetch the 10 most recent public challenges.
+ */
+const fetchChallenges: Fetcher<Challenge[]> = async (
+  key: string,
+  own = false
+) => {
+  const urlObj = new URL("/api/c", window.location.href);
+  if (own) urlObj.searchParams.append("own", "true");
+  const res = await fetch(urlObj, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => res.json());
+    headers: { "Content-Type": "application/json" },
+  });
+  return await res.json();
 };
 
 export default fetchChallenges;
