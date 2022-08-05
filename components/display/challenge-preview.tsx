7b8ac1b7e9c08977a75475a17d791bdfa63b21e6
Will Kerry <mail@example.com>
2022-08-05 09:32:36 +0100

    Add admin controls when user owns

--- components/display/challenge-preview.tsx
+++ components/display/challenge-preview.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   Card,
   Code,
+  Tooltip,
   Group,
   Text,
   useMantineTheme,
@@ -10,11 +11,44 @@ import React from "react";
 import Router from "next/router";
 import type { Challenge } from "@prisma/client";
 import LanguageIndicator from "./language-indicator";
+import AdminControls from "../inputs/admin-controls";
+import { deleteHandler, editHandler } from "../../utils/run/handlers";
 
-const ChallengePreview: React.FC<{ post: Challenge }> = ({ post }) => {
+type ChallengePreviewProps = {
+  challenge: Challenge;
+  userOwns?: boolean;
+};
+
+const ChallengePreview: React.FC<ChallengePreviewProps> = ({
+  challenge,
+  userOwns,
+}) => {
   const theme = useMantineTheme();
+
+  const adminControlProps: React.ComponentProps<typeof AdminControls> = {
+    id: challenge.id,
+    disabled: userOwns === false,
+    handleDelete: deleteHandler(challenge),
+    handleEdit: editHandler(challenge),
+  };
+
+  const controls = [
+    <Button
+      key="run"
+      onClick={() => Router.push("/c/[id]", `/c/${challenge.id}`)}
+      variant="default"
+      fullWidth={!userOwns}
+    >
+      Run
+    </Button>,
+  ];
+
+  if (userOwns)
+    controls.unshift(<AdminControls key="admin" {...adminControlProps} />);
+
   return (
     <Card shadow="sm" p="lg">
+      {/* Code Preview */}
       <Card.Section
         style={{
           backgroundColor: theme.colors.dark[8],
@@ -29,26 +63,31 @@ const ChallengePreview: React.FC<{ post: Challenge }> = ({ post }) => {
             color: theme.colors.dark[2],
             fontSize: theme.fontSizes.xs,
             userSelect: "none",
+            overflow: "hidden",
           })}
         >
-          {post.skeleton}
+          {challenge.skeleton}
         </Code>
       </Card.Section>
+
+      {/* Title */}
       <Group
         position="apart"
         style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+        noWrap
       >
-        <Text weight={500}>{post.title}</Text>
-        <LanguageIndicator language={post.language} compact />
+        <Tooltip label={challenge.title}>
+          <Text weight={500} lineClamp={1}>
+            {challenge.title}
+          </Text>
+        </Tooltip>
+        <LanguageIndicator language={challenge.language} compact />
       </Group>
-      <Button
-        onClick={() => Router.push("/c/[id]", `/c/${post.id}`)}
-        variant="default"
-        fullWidth
-        mt="lg"
-      >
-        Run
-      </Button>
+
+      {/* Actions */}
+      <Group mt="lg" position="apart">
+        {controls}
+      </Group>
     </Card>
   );
 };
