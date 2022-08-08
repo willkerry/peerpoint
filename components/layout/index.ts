44a232ac248d9f4c967a52b765130034d3d532c8
Will Kerry <mail@example.com>
2022-08-08 18:30:55 +0100

    Create title stuff component

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,5 +1,6 @@
 import Header from "./header";
 import Layout from "./layout";
 import Meta from "./meta";
+import TitleGroup from "./title-group";
 
-export { Header, Layout, Meta };
+export { Header, Layout, Meta, TitleGroup };
---
+++ components/layout/title-group.tsx
@@ -0,0 +1,25 @@
+import { Box, Text, Group, Title, Skeleton } from "@mantine/core";
+import { IdButton } from "../display";
+
+type TitleGroupProps = {
+  title: string;
+  id?: string;
+  area: string;
+};
+
+const TitleGroup: React.FC<TitleGroupProps> = ({ title, area, id }) => (
+  <Box>
+    <Text weight={500} color="dimmed">
+      {area}
+    </Text>
+    <Group position="apart" mb={12}>
+      <Skeleton visible={!title} width="60%" height={35}>
+        <Title order={2}>{title}</Title>
+      </Skeleton>
+
+      {id ? <IdButton {...{ id }} /> : null}
+    </Group>
+  </Box>
+);
+
+export default TitleGroup;
