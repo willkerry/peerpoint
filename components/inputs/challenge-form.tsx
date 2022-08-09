19c3ce66923329a9443cd7a68575d8b45498c4f9
Will Kerry <mail@example.com>
2022-08-09 10:57:50 +0100

    Create component form for challenge create/edit

---
+++ components/inputs/challenge-form.tsx
@@ -0,0 +1,142 @@
+import { UseFormReturnType } from "@mantine/form";
+import { useRouter } from "next/router";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { CreateFormValues } from "../../pages/create";
+import { quickExecuteAndPopulate } from "../../utils/create/form-handlers";
+import { Var } from "../display";
+import { Button, Grid, Group, Text, TextInput } from "@mantine/core";
+import { openConfirmModal } from "@mantine/modals";
+import { LanguageSelect } from ".";
+import dynamic from "next/dynamic";
+
+const CodeEditor = dynamic(() => import("../inputs/code-editor"), {
+  ssr: false,
+});
+
+type ChallengeFormComponentProps = {
+  form: UseFormReturnType<CreateFormValues>;
+  onSubmit: (values: CreateFormValues) => Promise<void>;
+  executing: boolean;
+  setExecuting: Dispatch<SetStateAction<boolean>>;
+  submitting: boolean;
+  setSubmitting: Dispatch<SetStateAction<boolean>>;
+  success: boolean;
+  setSuccess: Dispatch<SetStateAction<boolean>>;
+};
+
+const ChallengeForm: React.FC<ChallengeFormComponentProps> = (props) => {
+  const router = useRouter();
+  const runAndPopulate = quickExecuteAndPopulate(
+    props.setExecuting,
+    props.form
+  );
+
+  useEffect(() => {
+    const successModalProps = {
+      title: "Challenge created",
+      children: (
+        <Text size="sm">
+          Your challenge <Var>{props.form.values.title}</Var> has been created.
+        </Text>
+      ),
+      labels: { confirm: "Create more", cancel: "Exit" },
+      onCancel: () => {
+        props.setSuccess(false);
+        router.back();
+      },
+      onConfirm: () => {
+        props.form.reset();
+        props.setSuccess(false);
+      },
+    };
+    if (props.success) openConfirmModal(successModalProps);
+  }, [props.success, props.form]);
+
+  const cancelModal = () => {
+    const props = {
+      title: "Are you sure you want to cancel?",
+      labels: { confirm: "Yes", cancel: "Return" },
+      onConfirm: () => {
+        router.back();
+      },
+      confirmProps: { color: "red" },
+    };
+    return openConfirmModal(props);
+  };
+  return (
+    <form onSubmit={props.form.onSubmit((values) => props.onSubmit(values))}>
+      <Grid>
+        <Grid.Col sm={8}>
+          <TextInput
+            autoFocus
+            required
+            label="Title"
+            type="text"
+            {...props.form.getInputProps("title")}
+          />
+        </Grid.Col>
+        <Grid.Col sm={4}>
+          <LanguageSelect form={props.form} />
+        </Grid.Col>
+        <Grid.Col sm={6}>
+          <CodeEditor
+            label="Skeleton"
+            language={props.form.values.language}
+            descriptionProps={{ sx: { minHeight: "2.5em" } }}
+            description="
+              This is what appears in students’ code editors. Include
+              instructions (commented out), and any necessary boilerplate or
+              variables."
+            required
+            {...props.form.getInputProps("skeleton")}
+          />
+          <Button
+            disabled={!props.form.values.skeleton}
+            onClick={runAndPopulate}
+            loading={props.executing}
+            type="button"
+            fullWidth
+            variant="default"
+            mt={12}
+          >
+            {props.executing
+              ? "Running program"
+              : "Run and populate Expected output"}
+          </Button>
+        </Grid.Col>
+        <Grid.Col sm={6}>
+          <CodeEditor
+            label="Expected output"
+            descriptionProps={{ sx: { minHeight: "2.5em" } }}
+            description="
+              The stdout of student submissions is (fuzzily) checked against
+              this value, and matches are considered successful."
+            required
+            {...props.form.getInputProps("expectedOutput")}
+          />
+        </Grid.Col>
+        <Grid.Col sm={12}>
+          <Text color="red" size="xs">
+            {props.form.errors.submit}
+          </Text>
+        </Grid.Col>
+        <Grid.Col sm={12}>
+          <Group>
+            <Button
+              loading={props.submitting}
+              disabled={props.executing}
+              type="submit"
+            >
+              Create
+            </Button>
+            <Button variant="subtle" onClick={cancelModal}>
+              Cancel
+            </Button>
+          </Group>
+        </Grid.Col>
+      </Grid>
+    </form>
+  );
+};
+
+export default ChallengeForm;
--- components/inputs/index.ts
+++ components/inputs/index.ts
@@ -1,6 +1,13 @@
+import ChallengeForm from "./challenge-form";
 import CodeEditor from "./code-editor";
 import FindChallenge from "./find-challenge";
 import LanguageSelect from "./language-select";
 import RunControls from "./run-controls";
 
-export { CodeEditor, FindChallenge, LanguageSelect, RunControls };
+export {
+  ChallengeForm,
+  CodeEditor,
+  FindChallenge,
+  LanguageSelect,
+  RunControls,
+};
