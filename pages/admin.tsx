915a2a35005f184cf5fabf0f0126852636371bb8
Will Kerry <mail@example.com>
2022-08-09 18:38:36 +0100

    Add placeholder sort/filter controls to Admin

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,20 +1,50 @@
-import { Grid } from "@mantine/core";
+import { Grid, Select } from "@mantine/core";
+import { useSession } from "next-auth/react";
 import useSWR from "swr";
+
+import { ChallengePreview } from "../components/display/";
 import { Layout, TitleGroup } from "../components/layout";
 import fetchChallenges from "../lib/fetchers/fetch-challenges";
-import { ChallengePreview } from "../components/display/";
-import { useSession } from "next-auth/react";
 
 const Admin: React.FC = () => {
   const { data } = useSWR(["", true], fetchChallenges);
   const { data: session } = useSession();
-  console.log(session);
 
   return (
     <Layout loading={!data} title="Admin">
       <Grid>
         <Grid.Col span={12}>
           <TitleGroup area="Your challenges" title={session?.user?.email} />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Select
+            disabled
+            label="Filter by course"
+            defaultValue="all"
+            data={[
+              {
+                value: "all",
+                label: "All",
+              },
+            ]}
+          />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Select
+            disabled
+            label="Sort"
+            defaultValue="newest"
+            data={[
+              {
+                value: "newest",
+                label: "Newest",
+              },
+              {
+                value: "oldest",
+                label: "Oldest",
+              },
+            ]}
+          />
         </Grid.Col>
         {data?.map((ch) => (
           <Grid.Col span={12} xs={6} sm={4} md={3} xl={2} key={ch.id}>
