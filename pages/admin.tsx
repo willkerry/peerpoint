9a0fa850253d5e1b09369439cdb73a0a788b5306
Will Kerry <mail@example.com>
2022-08-09 08:55:22 +0100

    Fix: employ components consistently

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,19 +1,21 @@
-import { Title, Grid } from "@mantine/core";
+import { Grid } from "@mantine/core";
 import useSWR from "swr";
-import { Layout } from "../components/layout";
+import { Layout, TitleGroup } from "../components/layout";
 import fetchChallenges from "../lib/fetchers/fetch-challenges";
 import { ChallengePreview } from "../components/display/";
+import { useSession } from "next-auth/react";
 
 const Admin: React.FC = () => {
   const { data } = useSWR(["", true], fetchChallenges);
+  const { data: session } = useSession();
+  console.log(session);
 
   return (
     <Layout loading={!data} title="Admin">
-      <Title mb="xl" order={3}>
-        Your challenges
-      </Title>
-
       <Grid>
+        <Grid.Col span={12}>
+          <TitleGroup area="Your challenges" title={session?.user?.email} />
+        </Grid.Col>
         {data?.map((ch) => (
           <Grid.Col span={12} xs={6} sm={4} md={3} xl={2} key={ch.id}>
             <ChallengePreview challenge={ch} userOwns />
