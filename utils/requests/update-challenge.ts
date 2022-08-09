bf90b4a398a72f9b61b53d48913ee9e88c06bb3e
Will Kerry <mail@example.com>
2022-08-09 11:49:27 +0100

    Create barebones update request handler

---
+++ utils/requests/update-challenge.ts
@@ -0,0 +1,18 @@
+import Router from "next/router";
+import { CreateFormValues } from "../form-handlers/create-form-handlers";
+
+/** Send an update request (*safely* – auth happens server-side). */
+async function updateChallenge(
+  id: number,
+  values: CreateFormValues
+): Promise<void> {
+  if (isNaN(id)) throw new Error("Invalid post ID.");
+  await fetch(`/api/c/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(values),
+  });
+  await Router.push("/");
+}
+
+export default updateChallenge;
