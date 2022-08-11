00b9525eedd173ba54f6fafb35532ae198c2cc49
Will Kerry <mail@example.com>
2022-08-11 13:14:54 +0100

    Fix: more title group flexibility

--- components/layout/title-group.tsx
+++ components/layout/title-group.tsx
@@ -1,4 +1,4 @@
-import { Box, Group, Skeleton, Text, Title } from "@mantine/core";
+import { Grid, Skeleton, Text, Title, TitleOrder } from "@mantine/core";
 
 import { IdButton } from "../display";
 
@@ -6,22 +6,36 @@ type TitleGroupProps = {
   title: string;
   id?: string;
   area: string;
+  compact?: boolean;
 };
 
-const TitleGroup: React.FC<TitleGroupProps> = ({ title, area, id }) => (
-  <Box>
-    <Text weight={500} color="dimmed">
-      {area}
-    </Text>
-    <Group position="apart" mb={12} noWrap>
-      <Skeleton visible={!title} sx={{ minHeight: 35 }}>
-        {/* If title is 20+ chars and there's an ID, reduce to order 3.  */}
-        <Title order={title?.length >= 20 && id ? 3 : 2}>{title}</Title>
-      </Skeleton>
-
-      {id ? <IdButton {...{ id }} /> : null}
-    </Group>
-  </Box>
-);
+const TitleGroup: React.FC<TitleGroupProps> = ({
+  title,
+  area,
+  id,
+  compact,
+}) => {
+  let titleOrder = 2;
+  if (compact) titleOrder++;
+  if (title?.length > 20) titleOrder++;
+  if (title?.length > 30) titleOrder++;
+  return (
+    <Grid gutter={0} justify="space-between">
+      <Grid.Col span={12}>
+        <Text weight={500} color="dimmed">
+          {area}
+        </Text>
+      </Grid.Col>
+      <Grid.Col span={9}>
+        <Skeleton visible={!title} sx={{ minHeight: 20 }}>
+          <Title order={titleOrder as TitleOrder}>{title}</Title>
+        </Skeleton>
+      </Grid.Col>
+      <Grid.Col span={3} sx={{ textAlign: "right" }}>
+        {id ? <IdButton {...{ id }} /> : null}
+      </Grid.Col>
+    </Grid>
+  );
+};
 
 export default TitleGroup;
