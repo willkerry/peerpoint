530b7e7116e26e8c295413846b27545734e34f72
Will Kerry <will@willkerry.com>
2022-08-13 14:56:26 +0100

    Fix: start reducing number of import cycles

--- components/display/index.ts
+++ components/display/index.ts
@@ -1,22 +1,8 @@
-import ChallengePreview from "./challenge-preview";
 import DisplayId from "./display-id";
-import Edit from "./edit";
 import EmptyState from "./empty-state";
 import IdButton from "./id-button";
 import InfoModal from "./info-modal";
 import LanguageIndicator from "./language-indicator";
-import { Area, Pie } from "./monitor";
 import { Var } from "./variable";
 
-export {
-  Area,
-  ChallengePreview,
-  DisplayId,
-  Edit,
-  EmptyState,
-  IdButton,
-  InfoModal,
-  LanguageIndicator,
-  Pie,
-  Var,
-};
+export { DisplayId, EmptyState, IdButton, InfoModal, LanguageIndicator, Var };
--- components/display/monitor/index.ts
+++ components/display/monitor/index.ts
@@ -1,9 +1,4 @@
-import { MonitorResponse } from "../../../pages/api/c/monitor/[id]";
-import Area from "./charts/area";
-import Pie from "./charts/pie";
 import PlaceholderAvatar from "./placeholder-avatar";
 import SubmissionFeed from "./submission-feed/submission-feed";
 
-export { Area, Pie, PlaceholderAvatar, SubmissionFeed };
-
-export type ChartProps = { loading: boolean; data: MonitorResponse };
+export { PlaceholderAvatar, SubmissionFeed };
