5d747f947e61f48d8e8f0edde200b27cc293dc07
Will Kerry <will@willkerry.com>
2022-08-13 14:57:06 +0100

    Refactor admin controls component

--- components/inputs/admin-controls.tsx
+++ components/inputs/admin-controls.tsx
@@ -16,44 +16,46 @@ const AdminControls: React.FC<AdminControlsProps> = ({
   disabled,
   handleDelete,
   handleEdit,
-}) => {
-  return (
-    <Group spacing={6}>
-      <Tooltip label="Delete challenge">
-        <ActionIcon
-          color="red"
-          variant="light"
-          onClick={handleDelete}
-          {...{ disabled }}
-        >
-          <IconTrash size={16} />
-        </ActionIcon>
-      </Tooltip>
+}: AdminControlsProps) => (
+  <Group spacing={6}>
+    <Tooltip label="Delete challenge">
+      <ActionIcon
+        color="red"
+        variant="light"
+        onClick={handleDelete}
+        {...{ disabled }}
+      >
+        <IconTrash size={16} />
+      </ActionIcon>
+    </Tooltip>
 
-      <Tooltip label="Edit challenge">
+    <Tooltip label="Edit challenge">
+      <ActionIcon
+        variant="light"
+        color="blue"
+        onClick={handleEdit}
+        {...{ disabled }}
+      >
+        <IconEdit size={16} />
+      </ActionIcon>
+    </Tooltip>
+    <Tooltip label="Monitor responses">
+      <div>
         <ActionIcon
+          component={NextLink}
+          href={`../c/${id}/monitor`}
           variant="light"
-          color="blue"
-          onClick={handleEdit}
-          {...{ disabled }}
+          color="green"
         >
-          <IconEdit size={16} />
+          <IconHeartRateMonitor size={16} />
         </ActionIcon>
-      </Tooltip>
-      <Tooltip label="Monitor responses">
-        <div>
-          <ActionIcon
-            component={NextLink}
-            href={`../c/${id}/monitor`}
-            variant="light"
-            color="green"
-          >
-            <IconHeartRateMonitor size={16} />
-          </ActionIcon>
-        </div>
-      </Tooltip>
-    </Group>
-  );
+      </div>
+    </Tooltip>
+  </Group>
+);
+
+AdminControls.defaultProps = {
+  disabled: false,
 };
 
 export default AdminControls;
