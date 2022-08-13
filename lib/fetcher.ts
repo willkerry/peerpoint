54f340a97b553e13c1303e09fcbc8e70744447b1
Will Kerry <will@willkerry.com>
2022-08-13 14:58:02 +0100

    Fix: fetcher consistency

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,17 +1,17 @@
 import { Fetcher } from "swr";
+
 import { SubmissionRequest } from "../types/Submission";
 
 /**
  * Pass a URL and a SubmissionRequest object to this function, and it will perform a POST request to the URL.
  */
-const fetcher: Fetcher = (url: string, body: SubmissionRequest) => {
-  return fetch(url, {
+const fetcher: Fetcher = (url: string, body: SubmissionRequest) =>
+  fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   }).then((res) => res.json());
-};
 
 export default fetcher;
--- lib/getter.ts
+++ lib/getter.ts
@@ -1,17 +1,17 @@
 import { Fetcher } from "swr";
+
 import { SubmissionRequest } from "../types/Submission";
 
 /**
  * Pass a URL and a SubmissionRequest object to this function, and it will perform a GET request to the URL.
  */
-const getter: Fetcher = (url: string, body: SubmissionRequest) => {
-  return fetch(url, {
+const getter: Fetcher = (url: string, body: SubmissionRequest) =>
+  fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   }).then((res) => res.json());
-};
 
 export default getter;
