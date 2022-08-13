97c16f504659cd2a1ba81b1543d6675bf1b9ee27
Will Kerry <will@willkerry.com>
2022-08-13 11:06:57 +0100

    Fix: declare variable before using it

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,9 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextApiHandler } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import EmailProvider from "next-auth/providers/email";
-import prisma from "../../../lib/prisma";
 
-const authHandler: NextApiHandler = (req, res) =>
-  NextAuth(req, res, authOptions);
-export default authHandler;
+import prisma from "../../../lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -25,3 +22,7 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
+
+const authHandler: NextApiHandler = (req, res) =>
+  NextAuth(req, res, authOptions);
+export default authHandler;
