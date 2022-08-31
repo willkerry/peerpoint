f9500952625dc04c9bc4c95f1d8e8b94d1105353
Will Kerry <will@willkerry.com>
2022-08-31 12:03:36 +0100

    Remove unused layout components

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-// import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 import { AppShell, Box, Group, Header as MantineHeader } from "@mantine/core";
@@ -23,44 +22,18 @@ const defaultProps = {
   noPad: false,
 };
 
-const Layout: React.FC<Props> = ({ title, children, loading, noPad }) => (
-  /*
-   * const theme = useMantineTheme();
-   * const [opened, setOpened] = React.useState(false);
-   */
-  // const { data: session } = useSession();
-
-  /*
-   * const sidebar = (
-   *   <Navbar
-   *     p="md"
-   *     hiddenBreakpoint="sm"
-   *     // hidden={!opened}
-   *     width={{ sm: 200, lg: 300 }}
-   *   >
-   *     <Navbar.Section grow>
-   *       {session && (
-   *         <Stack>
-   *           <Link href="/" passHref>
-   *             <Anchor component="a">Feed</Anchor>
-   *           </Link>
-   *           <Link href="/drafts" passHref>
-   *             <Anchor component="a">Drafts</Anchor>
-   *           </Link>
-   *         </Stack>
-   *       )}
-   *     </Navbar.Section>
-   *   </Navbar>
-   * );
-   */
-
+const Layout: React.FC<Props> = ({
+  title,
+  children,
+  loading,
+  noPad,
+}: Props) => (
   <>
     <Meta {...{ title }} />
     <AppShell
       navbarOffsetBreakpoint="sm"
       fixed
       padding={noPad ? 0 : "md"}
-      // navbar={sidebar}
       header={
         <MantineHeader height={50} p="md">
           <Box
@@ -70,15 +43,6 @@ const Layout: React.FC<Props> = ({ title, children, loading, noPad }) => (
               height: "100%",
             }}
           >
-            {/* <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery> */}
             <Group position="apart" grow style={{ width: "100%" }}>
               <Link href="/" passHref>
                 <Box component="a" sx={{ color: "inherit", lineHeight: 0 }}>
